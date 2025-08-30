656b9f3ff3ddddd50dcd6612f9a75c92c4e94551
OMKAR9395 <omkarmohite85@gmail.com>
2025-08-30 11:19:10 +0530

    Crud

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { appConfig } from './app.config';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:AppComponent
+    },
+    {
+        path:'app',
+        component:AppComponent
+    }
+];
